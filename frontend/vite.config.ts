047a5b4d3c3cc1391fde2a04129d442b5b91f0c4
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-06-24 09:21:23 +0300

    fix: tailwind integration with vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
@@ -19,11 +20,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
   build: {
     sourcemap: true,
-    outDir: 'dist',
+    outDir: "dist",
   },
-})
+});
